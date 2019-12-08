ef56574631447fc5d188d2f8f3cc4b1b45c3f3d6
J. Soto <jsoto3070@gmail.com>
2019-12-08 14:40:37 -0500

    Added README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# Regex Scraper
+Regex Scraper
--- regex_phone_email.py
+++ regex_phone_email.py
@@ -34,3 +34,10 @@ for groups in emailRegex.findall(text):
     matches.append(groups[0])
 
 # TODO: Copy results to the clipboard.
+
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses found.')
